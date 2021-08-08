53aa6403edcf1eb56ff66f2b43bc58a58d26bc8b
brent knop <brent@maxl-rose.lan>
2021-08-08 18:07:59 -0400

    Builds in docker container

--- sna-api/api/Models/Organization.cs
+++ sna-api/api/Models/Organization.cs
@@ -5,7 +5,6 @@ namespace api.Models
     {
         public int OrganizationId { get; set; }
         public string Specialty { get; set; }
-        public long Npi { get; set; }
 
         public Organization()
         {
--- sna-api/api/Models/Provider.cs
+++ sna-api/api/Models/Provider.cs
@@ -8,9 +8,12 @@ namespace api.Models
     public class Provider
     {
         public int ProviderId { get; set; }
-        public string Name { get; set; }
-
-        /* Do these go on Provider or Organization */
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int ZipCode { get; set; }
         public string Specialty { get; set; }
         public long Npi { get; set; }
 
